9d404f7a3b16e47db7e2cb017e5abd5abb76c69b
Hackers Tech <83658615+HackersTech@users.noreply.github.com>
2021-06-19 20:11:17 +0530

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,9 +1,9 @@
 #
 cd ..
 rm -rf 
-git clone 
-cd 
+git clone https://github.com/HackersTech/crispy-bash-utilities
+cd crispy-bash-utilities
 echo "updated. !!!" | lolcat
-sleep 2
+sleep 3
 bash b.sh
 
